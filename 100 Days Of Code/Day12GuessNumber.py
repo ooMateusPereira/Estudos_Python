ba0eb491a83eebbae558b3004b8fc69edda54f03
ooMateusPereira <mateusufc1@live.com>
2025-06-26 17:53:21 -0300

    feat: hard level added to Guess a Number game.

--- 100 Days Of Code/Day12GuessNumber.py
+++ 100 Days Of Code/Day12GuessNumber.py
@@ -1,37 +1,57 @@
-import random
-from winsound import PlaySound
-from art import tprint
-tprint('Guess  the  Number')
-
-RANDOM_NUMBER = random.randint(1, 100)
-
-
 def GuessNumber():
+    import random
+    from art import tprint
+    tprint('Guess  the  Number')
 
+    RANDOM_NUMBER = random.randint(1, 100)
+    
     print('You have 5 attempts to discover the number between 1 and 100.\n')
 
     def easy():
-            LIVES = 5
-            PLAYNG = True
+        LIVES = 5
+        PLAYNG = True
+        
+        while PLAYNG:
+            pick_numb = int(input(f'Pick a number. You have more {LIVES} attempts.\n'))
 
-            pick_numb = int(input('Pick a number\n'))
-            while PLAYNG:
-                if pick_numb == RANDOM_NUMBER:
-                    print('Good job! You guess the number!')
-                    PLAYNG = False
-                else:
-                    LIVES -= 1
-                    pick_numb = int(input(f'Pick another number. You have more {LIVES} attempts.\n'))
-                
-                if pick_numb != RANDOM_NUMBER:
-                    if LIVES == 0:
-                        PLAYNG = False
-                        print(f'You lose! The right was {RANDOM_NUMBER}')
+            if pick_numb == RANDOM_NUMBER:
+                print('Good job! You guess the number!')
+                PLAYNG = False
+            else:
+                LIVES -= 1
+            
+            if pick_numb > RANDOM_NUMBER:
+                print('To high!')
+            else: 
+                print('To low!')
 
-                        
+            if LIVES == 0:
+                PLAYNG = False
+                print(f'You lose! The right was {RANDOM_NUMBER}')
+
+
     def hard():
-        print('You have 5 attempts')
+        
+        LIVES = 5
+        PLAYNG = True
+        
+        while PLAYNG:
+            pick_numb = int(input(f'Pick a number. You have more {LIVES} attempts.\n'))
 
+            if pick_numb == RANDOM_NUMBER:
+                print('Good job! You guess the number!')
+                PLAYNG = False
+            else:
+                LIVES -= 1
+            
+            if pick_numb > RANDOM_NUMBER:
+                print('To high!')
+            else: 
+                print('To low!')
+
+            if LIVES == 0:
+                PLAYNG = False
+                print(f'You lose! The right was {RANDOM_NUMBER}')
 
     print('Welcome to the Guess the number game!')
     level = input("\nChoose a difficulty. Type 'easy' or 'hard'. \n").lower()
