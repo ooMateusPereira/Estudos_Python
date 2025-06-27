f0d86b5e63be936c042bc44a16e19fd72e90d648
ooMateusPereira <mateusufc1@live.com>
2025-06-27 17:48:02 -0300

    feat: adding more exercises.

--- 100 Days Of Code/Day 13/Day13.py
+++ 100 Days Of Code/Day 13/Day13.py
@@ -28,3 +28,30 @@ except ValueError:
 if age > 18:
     print('You can drive now.')
 
+# Target is the number up to which we count
+def fizz_buzz(target):
+    number = []
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        if number % 3 == 0:
+            print("Fizz")
+        if number % 5 == 0:
+            print("Buzz")
+        else:
+            print([number])
+
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        # use and (not or)
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        # use elif (not if)
+        elif number % 3 == 0:
+            print("Fizz")
+        # use elif (not if)
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            # remove square brackets []
+            print(number)
