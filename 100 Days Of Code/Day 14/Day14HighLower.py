595a5aa418254d574c60e0228f5e383355be4b60
Mateus Pereira da Silva <mateusufc1@live.com>
2025-06-28 18:12:29 -0300

    feat: add logo and data for dictionary.

---
+++ 100 Days Of Code/Day 14/Day14HighLower.py
@@ -0,0 +1,6 @@
+import random 
+from Dictionary import data
+from Logo import art1
+from Logo import art2
+
+print(art1)
---
+++ 100 Days Of Code/Day 14/Dictionary.py
@@ -0,0 +1,182 @@
+data = [
+    {
+        'name': 'Neymar',
+        'followers_count': 200,
+        'description': 'Soccer player',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Ariana Grande',
+        'followers_count': 200,
+        'description': 'Singer and actress',
+        'country': 'USA'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'followers_count': 600,
+        'description': 'Soccer player',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Lionel Messi',
+        'followers_count': 500,
+        'description': 'Soccer player',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Selena Gomez',
+        'followers_count': 430,
+        'description': 'Singer and actress',
+        'country': 'USA'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'followers_count': 360,
+        'description': 'Media personality',
+        'country': 'USA'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'followers_count': 400,
+        'description': 'Actor and wrestler',
+        'country': 'USA'
+    },
+    {
+        'name': 'Kylian Mbappé',
+        'followers_count': 120,
+        'description': 'Soccer player',
+        'country': 'France'
+    },
+    {
+        'name': 'Taylor Swift',
+        'followers_count': 280,
+        'description': 'Singer and songwriter',
+        'country': 'USA'
+    },
+    {
+        'name': 'Billie Eilish',
+        'followers_count': 110,
+        'description': 'Singer and songwriter',
+        'country': 'USA'
+    },
+    {
+        'name': 'Zendaya',
+        'followers_count': 180,
+        'description': 'Actress and singer',
+        'country': 'USA'
+    },
+    {
+        'name': 'Shakira',
+        'followers_count': 90,
+        'description': 'Singer and dancer',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Drake',
+        'followers_count': 140,
+        'description': 'Rapper and singer',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Rihanna',
+        'followers_count': 150,
+        'description': 'Singer and businesswoman',
+        'country': 'Barbados'
+    },
+    {
+        'name': 'David Beckham',
+        'followers_count': 85,
+        'description': 'Retired soccer player',
+        'country': 'England'
+    },
+    {
+        'name': 'Emma Watson',
+        'followers_count': 75,
+        'description': 'Actress and activist',
+        'country': 'UK'
+    },
+    {
+        'name': 'Manuel Neuer',
+        'followers_count': 12,
+        'description': 'Soccer player',
+        'country': 'Germany'
+    },
+    {
+        'name': 'BTS',
+        'followers_count': 80,
+        'description': 'K-pop group',
+        'country': 'South Korea'
+    },
+    {
+        'name': 'Lewis Hamilton',
+        'followers_count': 35,
+        'description': 'Formula 1 driver',
+        'country': 'UK'
+    },
+    {
+        'name': 'Camila Cabello',
+        'followers_count': 70,
+        'description': 'Singer and songwriter',
+        'country': 'Cuba/USA'
+    },
+    {
+        'name': 'Vinicius Jr.',
+        'followers_count': 60,
+        'description': 'Soccer player',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Anitta',
+        'followers_count': 65,
+        'description': 'Singer and performer',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Whindersson Nunes',
+        'followers_count': 59,
+        'description': 'Comedian and influencer',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Juliette',
+        'followers_count': 30,
+        'description': 'Singer and ex-BBB',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Larissa Manoela',
+        'followers_count': 48,
+        'description': 'Actress and singer',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Alok',
+        'followers_count': 28,
+        'description': 'DJ and music producer',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Luan Santana',
+        'followers_count': 34,
+        'description': 'Singer',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Virginia Fonseca',
+        'followers_count': 45,
+        'description': 'Influencer and entrepreneur',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Leonardo DiCaprio',
+        'followers_count': 60,
+        'description': 'Actor',
+        'country': 'USA'
+    },
+    {
+        'name': 'Tom Holland',
+        'followers_count': 70,
+        'description': 'Actor',
+        'country': 'UK'
+    }
+]
