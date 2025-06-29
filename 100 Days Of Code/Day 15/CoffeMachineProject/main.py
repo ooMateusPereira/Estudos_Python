9ecb2a5262b38d7be7b94e44ae3aa7efbcb061c7
Mateus Pereira da Silva <mateusufc1@live.com>
2025-06-29 15:32:14 -0300

    feat: coffee machine improvements.

--- 100 Days Of Code/Day 15/CoffeMachineProject/main.py
+++ 100 Days Of Code/Day 15/CoffeMachineProject/main.py
@@ -1,7 +1,6 @@
 from art import tprint
 tprint('Coffee   Machine')
 
-machine = True
 type_coffee = input("What would you like? (espresso/latte/cappuccino)  ")
 
 supplies = {
@@ -11,26 +10,63 @@ supplies = {
     'money': 0
 }
 
-def decision():
-    if type_coffee == 'espresso':
-        supplies['water'] -= 100
-        supplies['coffe'] -= 15
-    elif type_coffee == 'latte':
-        supplies['water'] -= 100
-        supplies['milk'] -= 100
-        supplies['coffee'] -= 25
-    elif type_coffee == 'capuccino':
-        supplies['water'] -= 100
-        supplies['milk'] -= 200
-        supplies['coffee'] -= 25
-    elif type_coffee == 'report':
-        print(f"Wather:", supplies['water'],
-            '\nMilk:  ', supplies['milk'], 
-            '\nCoffee:', supplies['coffee'], 
-            '\nMoney:', '$', supplies['money'])
-    elif type_coffee == 'off':
-        print('Bye! Have a good coffee.')
-        machine = False
+def report():
+    print(supplies['water'],'\n',
+          supplies['milk'], '\n',
+          supplies['coffee'], '\n',
+          supplies['money'])
 
-def check_supplies():
+def espresso(type_coffee, supplies): 
+    def report():
+        if type_coffee == 'espresso' and supplies['water'] >= 100 and supplies['coffee'] >= 15:
+            supplies['water'] -= 100
+            supplies['coffee'] -= 15
+            print('Enjoy your Espresso!')   
+        else:
+            print("Sorry. We don't have enough supplies.")
     
+    report()
+
+def latte(type_coffee, supllies):
+    def report():
+        if type_coffee == 'latte'and supplies['water'] >= 100 and supplies['milk'] >= 100 and supplies['coffee'] <= 25:    
+            supplies['water'] -= 100
+            supplies['milk'] -= 100
+            supplies['coffee'] -= 25
+            print('Enjoy your Latte!')
+        else:
+            print( "Sorry. We don't have enough supplies.")
+            
+    report()
+
+def capuccino(type_coffee, supllies):
+    def report():
+        if type_coffee == 'capuccino'and supplies['water'] >= 100 and supplies['milk'] >=200 and supplies['coffee'] <= 25:
+            supplies['water'] -= 100
+            supplies['milk'] -= 200
+            supplies['coffee'] -= 25
+            print("Enjoy your Capuccino!")
+        else:
+            print( "Sorry. We don't have enough supplies.")
+    report()
+
+def decision():
+    machine = True
+    while machine:
+        type_coffee = input("What would you like? (espresso/latte/cappuccino)  ")
+        
+        if type_coffee == 'espresso':
+            espresso(type_coffee, supplies)
+        elif type_coffee == 'latte':
+            latte(type_coffee, supplies)
+        elif type_coffee == 'capuccino':
+            capuccino(type_coffee, supplies)
+        elif type_coffee == 'report':
+            report()
+        elif type_coffee == 'off':
+            print('Bye! Have a good coffee.')
+            machine = False
+        else:
+            print('Invalid option.')
+    
+decision()
