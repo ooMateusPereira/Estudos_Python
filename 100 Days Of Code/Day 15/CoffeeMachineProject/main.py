850ac6da0a34e22fa5ce7f3fe38b942fda6fc91e
Mateus Pereira da Silva <mateusufc1@live.com>
2025-06-30 18:18:43 -0300

    feat: added money logic in Coffee Machine Project.

--- 100 Days Of Code/Day 15/CoffeeMachineProject/main.py
+++ 100 Days Of Code/Day 15/CoffeeMachineProject/main.py
@@ -9,14 +9,20 @@ supplies = {
     'money': 0
 }
 
+
+
+
+
+
+
 def espresso(type_coffee, supplies): 
     def report():
         if type_coffee == 'espresso' and supplies['water'] >= 100 and supplies['coffee'] >= 15:
             supplies['water'] -= 100
             supplies['coffee'] -= 15
-            print('Enjoy your Espresso!')   
+            print('\nEnjoy your Espresso!')   
         else:
-            print("Sorry. We don't have enough supplies.")
+            print("\nSorry. We don't have enough supplies.")
     
     report()
 
@@ -26,9 +32,9 @@ def latte(type_coffee, supplies):
             supplies['water'] -= 100
             supplies['milk'] -= 100
             supplies['coffee'] -= 25
-            print('Enjoy your Latte!')
+            print('\nEnjoy your Latte!')
         else:
-            print("Sorry. We don't have enough supplies.")
+            print("\nSorry. We don't have enough supplies.")
             
     report()
 
@@ -38,9 +44,9 @@ def capuccino(type_coffee, supplies):
             supplies['water'] -= 100
             supplies['milk'] -= 200
             supplies['coffee'] -= 25
-            print("Enjoy your Capuccino!")
+            print("\nEnjoy your Capuccino!")
         else:
-            print("Sorry. We don't have enough supplies.")
+            print("\nSorry. We don't have enough supplies.")
     report()
 
 def report():
@@ -54,12 +60,17 @@ def decision():
     machine = True
     while machine:
         type_coffee = input("\nWhat would you like? (espresso/latte/capuccino): ").lower()
-        
-        if type_coffee == 'espresso':
+        quarters = int(input('Put yours quarters. ')) * 0.2
+        dime = int(input('Put yours dimes. ')) * 0.1
+        nickel = int(input('Put yours nickels. ')) * 0.05
+        penny = int(input('Put yours cents. ')) * 0.01
+        total_coins = penny + nickel + dime + quarters
+
+        if type_coffee == 'espresso' and total_coins >= 1.5:
             espresso(type_coffee, supplies)
-        elif type_coffee == 'latte':
+        elif type_coffee == 'latte' and total_coins >= 2.5:
             latte(type_coffee, supplies)
-        elif type_coffee == 'capuccino':
+        elif type_coffee == 'capuccino' and total_coins >= 3:
             capuccino(type_coffee, supplies)
         elif type_coffee == 'report':
             report()
@@ -67,6 +78,6 @@ def decision():
             print('Bye! Have a good coffee.')
             machine = False
         else:
-            print('Invalid option.')
+            print('Invalid option or insufficient money.')
 
 decision()
