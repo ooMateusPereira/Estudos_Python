81b91881e2534834045a994090babdc4d4170e65
Mateus Pereira da Silva <mateusufc1@live.com>
2025-07-02 22:59:26 -0300

    feat: quiz game is fineshed.

--- 100 Days Of Code/Day 17/quiz-game-start/main.py
+++ 100 Days Of Code/Day 17/quiz-game-start/main.py
@@ -13,4 +13,8 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions:
+    quiz.next_question()
+
+print("You've completed the quiz!")
--- 100 Days Of Code/Day 17/quiz-game-start/quiz_brain.py
+++ 100 Days Of Code/Day 17/quiz-game-start/quiz_brain.py
@@ -3,8 +3,22 @@ class QuizBrain:
     def __init__(self, q_list): 
         self.q_number = 0
         self.q_list =  q_list
+        self.score = 0
+    def still_has_questions(self):
+        return self.q_number <= len(self.q_list)
 
     def next_question(self):
         current_question = self.q_list[self.q_number]
         self.q_number += 1
-        input(f"Q.{self.q_number}: {current_question.text} (True/False)")
+        user_answer = input(f"Q.{self.q_number}: {current_question.text} (True/False)")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print('You got it right!')
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is {self.score}/{self.q_number}")
+        print('\n')
