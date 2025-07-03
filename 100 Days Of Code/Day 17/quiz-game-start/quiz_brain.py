5b266dd9cbad701223248ee61dd6dc9b90f690c4
ooMateusPereira <mateusufc1@live.com>
2025-07-03 09:52:52 -0300

    fix: fixed bug name.

--- 100 Days Of Code/Day 17/quiz-game-start/quiz_brain.py
+++ 100 Days Of Code/Day 17/quiz-game-start/quiz_brain.py
@@ -4,13 +4,14 @@ class QuizBrain:
         self.q_number = 0
         self.q_list =  q_list
         self.score = 0
+   
     def still_has_questions(self):
         return self.q_number <= len(self.q_list)
 
     def next_question(self):
         current_question = self.q_list[self.q_number]
         self.q_number += 1
-        user_answer = input(f"Q.{self.q_number}: {current_question.text} (True/False)")
+        user_answer = input(f"Q.{self.q_number}: {current_question.text} (True/False)  ")
         self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
