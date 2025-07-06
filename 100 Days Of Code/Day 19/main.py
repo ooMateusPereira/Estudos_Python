383f4dfe1de1fadd17e60f4b088cd40b950d6e24
Mateus Pereira da Silva <mateusufc1@live.com>
2025-07-06 14:01:56 -0300

    feat: code improvement

--- 100 Days Of Code/Day 19/main.py
+++ 100 Days Of Code/Day 19/main.py
@@ -2,20 +2,31 @@ import turtle
 
 tim = turtle.Turtle()
 my_screen = turtle.Screen()
+tim.speed('fastest')
 
-def move_up():
+def move():
     tim.forward(10)
 
+def move_back():
+    tim.forward(-10)
+
 def move_right():
-    tim.right(10)
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
 
 def move_left():
-    tim.left(10)
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
 
 my_screen.listen()
-my_screen.onkeypress(move_up, 'w')
+my_screen.onkeypress(move, 'w')
+my_screen.onkeypress(move_back, 's')
 my_screen.onkeypress(move_right, 'd')
 my_screen.onkeypress(move_left, 'a')
+my_screen.onkeypress(clear, 'c')
 
 print(my_screen.canvheight)
 my_screen.exitonclick()
