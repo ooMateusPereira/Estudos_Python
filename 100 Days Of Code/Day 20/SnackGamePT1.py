fa7bf5c89601ea569df4448f0164720dc4017519
Mateus Pereira da Silva <mateusufc1@live.com>
2025-07-07 16:58:07 -0300

    init: day 20

---
+++ 100 Days Of Code/Day 20/SnackGamePT1.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen, exitonclick
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor('black')
+screen.title('Snake Game')
+
+
+
+
+
+print(screen.canvheight)
+screen.exitonclick()
